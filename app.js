bfe38f50678112ba317e3342e8e4b022f08e667e
Ganzorig <Ganzorig6337@gmail.com>
2022-03-30 12:15:07 +0800

    Event listeneruudiig uusgej ogloo

---
+++ app.js
@@ -0,0 +1,36 @@
+//Дэлгэцтэй ажиллах контроллор
+var uiController = (function () {
+
+})();
+
+//Санхүүтэй ажиллах контроллор
+var financeController = (function () {
+
+
+
+})();
+
+//Програмын холбогч контроллор
+var appController = (function (uiController, financeController) {
+
+    var ctrlAddItem = function () {
+        // 1. Оруулах өгөгдлийг дэлгэцээс олж авна.
+        // 2. Олж авсан өгөгдлүүдээ financeController функцд дамжуулж тэнд хадгална.
+        // 3. Олж авсан өгөгдлүүдээ Вэб дээрээ тохирох хэсэгт нь гаргана.
+        // 4. Төсвийг тооцоолно.
+        // 5. Эцсийн үлдэгдэл тооцоог дэлгэцэнд гаргана. 
+    }
+
+    document.querySelector('.add__btn').addEventListener('click', function () {
+        ctrlAddItem();
+    });
+
+    document.addEventListener('keypress', function (event) {
+        if (event.keyCode === 13 || event.which === 13) {
+            ctrlAddItem();
+        }
+
+
+    });
+
+})(uiController, financeController); 
